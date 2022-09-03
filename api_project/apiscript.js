6900af9200c8180109883a406487a2d367ac2bb1
Christian Lozano <johndoe@example.com>
2022-09-03 08:55:47 -0500

    first commit

---
+++ api_project/apiscript.js
@@ -0,0 +1,20 @@
+
+
+console.log("Something tree related...")
+function tree(element){
+    const name = document.querySelector(".search").value;
+
+    $.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${name}`, function(data){
+        console.log(data[0].meanings[0].definitions[0].definition);
+        $('.result').text(data[0].meanings[0].definitions[0].definition);
+        }
+    )
+}
+// tree();
+
+
+
+
+// .definitions[0].definition
+// .definitions[0].definition
+// /en/tree
